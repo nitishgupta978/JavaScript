059a4a7e74e45540d9a66e057b29d292e818f6e2
nitishgupta978 <nitishgupta9785@gmail.com>
2023-01-18 16:52:42 +0530

    Added : Dice Game program

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 /*
-Count =  Counter program 
+Dies = three dies roll game
 */
 
-let count = 0;
+let x;
+let y;
+let z;
+let a;
 
-document.getElementById("increment").onclick = function () {
-    count += 1;
-    document.getElementById("countLable").innerHTML = count;
-}
+document.getElementById("roll").onclick = function () {
+    x = Math.floor(Math.random() * 6) + 1;
+    y = Math.floor(Math.random() * 6) + 1;
+    z = Math.floor(Math.random() * 6) + 1;
+    a = Math.abs(x + y + z);
 
-document.getElementById("reset").onclick = function () {
-    count = 0;
-    document.getElementById("countLable").innerHTML = count;
-}
 
-document.getElementById("decrement").onclick = function () {
-    count -= 1;
-    document.getElementById("countLable").innerHTML = count;
+    document.getElementById("xLable").innerHTML = x;
+    document.getElementById("yLable").innerHTML = y;
+    document.getElementById("zLable").innerHTML = z;
+    document.getElementById("aLable").innerHTML = "Sum of three Dice is : " + a;
 }
