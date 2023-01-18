c22f29d2a62822afb025e009826c2baa1e7cb465
nitishgupta978 <nitishgupta9785@gmail.com>
2023-01-18 15:03:48 +0530

    Added: User input Value

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 /*
-math 
+UserInput parameters
+*/
+let a;
+let b;
+let c;
+/*
+a = window.prompt("Enter number of Side A ");
+a = Number(a);
+
+b = window.prompt("Enter number of Side B ");
+b = Number(b);
+
+// c = Math.pow(a, 2)+ Math.pow(b,2);
+c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+console.log(c);
 */
 
-let x = 5.25;
+document.getElementById("myButton").onclick = function () {
+    a = document.getElementById("aText").value;
+    a = Number(a);
 
-// x = window.prompt("Enter your number: ");
-// x = Math.floor(x); // always give down value like 5
-// x = Math.ceil(x); // always give upper value like 6 
+    b = document.getElementById("bText").value;
+    b = Number(b);
 
-// x = Math.pow(x, 2); // for power 
-// x = Math.abs(x); // give positive value like -5.65= 5.65
-// x = Math.round(x); // if value >0.5 then increase 6 otherwise Decrease 5
-x = Math.sqrt(2);
-console.log(x);
-
-
-let a = 45;
-let b = 65;
-let c = 23.09;
-let maximum;
-let minimum;
-
-// maximum = Math.max(a, b, c);
-// console.log(maximum);
-
-minimum = Math.min(a, b, c);
-console.log(minimum);
-
-//   for PI constant value eg y = 3.141592653589793
-let y;
-y = Math.PI;
-console.log(y);
+    c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    c = document.getElementById("clable").innerHTML = " sqrt value of(sum of power of Side a and b) Side c is :" + c;
+}
