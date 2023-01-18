1a9312ca612ed122e063f318568eed948cbd5ae0
nitishgupta978 <nitishgupta9785@gmail.com>
2023-01-18 14:22:05 +0530

    Added : Math Function

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 /*
-const = a variable can't be change
+math 
 */
 
-const PI = 3.14159;
-let radius;
-let circumference;
+let x = 5.25;
 
-radius = window.prompt("Enter radius number: ");
-radius = Number(radius);
-// PI = 244.1;
-circumference = 2 * PI * radius;
-console.log(circumference);
+// x = window.prompt("Enter your number: ");
+// x = Math.floor(x); // always give down value like 5
+// x = Math.ceil(x); // always give upper value like 6 
 
+// x = Math.pow(x, 2); // for power 
+// x = Math.abs(x); // give positive value like -5.65= 5.65
+// x = Math.round(x); // if value >0.5 then increase 6 otherwise Decrease 5
+x = Math.sqrt(2);
+console.log(x);
+
+
+let a = 45;
+let b = 65;
+let c = 23.09;
+let maximum;
+let minimum;
+
+// maximum = Math.max(a, b, c);
+// console.log(maximum);
+
+minimum = Math.min(a, b, c);
+console.log(minimum);
+
+//   for PI constant value eg y = 3.141592653589793
+let y;
+y = Math.PI;
+console.log(y);
