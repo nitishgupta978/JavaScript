881f724a650ab2f0f9b4db6c199a01c3e62ce057
nitishgupta978 <nitishgupta9785@gmail.com>
2023-01-19 17:22:00 +0530

    Added : use variables and difference between both

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 /*
-ternary operator - it is shortcut of if/else statement
-                 take 3 steps
-                 1 . conditions
-                 2 . expression if true
-                 3 . expression if false
-               
-      condition? ifTrur : ifFalse;           
+variables operator - where a variable is accessible    
+
+1. let - variables are limited to the block scope{}, 
+2. var - variable are limited to the function scope(){},
+
+globle variable  = is declared outside any function
+"if globle, var will CHANGE browsers windows properties"
 */
 
-let adult = getValue(31);
-console.log("value is : ", adult);
-function getValue(age) {
-    return age >= 18 ? true : false;
+/*
+ for 1 let its persant in block scope
 
+for (let i = 0; i <= 4; i += 1) {
+    console.log(i);
+}
+// for 1 let its outside the block scope
+for (let i = 0; i <= 9; i += 1) {
+}
+console.log(i);
+
+*/
+let name = "John"; // globle 
+getValues();
+function getValues() {
+    for (var i = 0; i <= 4; i += 1) {
+
+    }
+    console.log(i);
 }
 
-Winners(false);
-function Winners(win) {
-    win ? console.log("You are WIN!") : console.log("You are LOSE!");
-}
 
+/* for 2 var its not work out side the function
+function getValues() {
+    for (i = 0; i <= 4; i += 1) {
+
+    }
+}
+console.log(i);
+
+*/
