18dd9ffc42eaed676b0b76b5e5a26fc849837799
nitishgupta978 <nitishgupta9785@gmail.com>
2023-01-19 15:41:52 +0530

    added : nestedloop  for rows and columns

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 /*
-break - breaks out of a loop entirely
-continue - skip an iteration of a loop            
+nested loop - a loop inside another loop
 */
 
 
 /*
-for (let i = 1; i <= 15; i += 1) {
-    if (i == 9) {
-        break;
+for (let i = 1; i <= 4; i += 1) {
+    for (let j = 1; j <= 4; j += 1) {
+        console.log(j);
     }
-    console.log(i);
+
 }
 */
-for (let i = 1; i <= 15; i += 1) {
-    if (i == 9) {
-        continue;
-    }
-    console.log(i);
-}
 
-console.log("Happy new years");
+
+let symbol = window.prompt("enter you favorite symbol!");
+let rows = window.prompt("enter row number!");
+let column = window.prompt("enter column number!");
+
+for (let i = 1; i <= rows; i += 1) {
+    for (let j = 1; j <= column; j += 1) {
+        document.getElementById("myLable").innerHTML += symbol;
+    }
+    document.getElementById("myLable").innerHTML += "<br>";
+}
