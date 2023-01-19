e302969203022fe6c8fa64c45653817970f21f97
nitishgupta978 <nitishgupta9785@gmail.com>
2023-01-19 15:19:16 +0530

    Added : break and continue operators

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 /*
-for loop - repeat some code a certain amount of time
-              
+break - breaks out of a loop entirely
+continue - skip an iteration of a loop            
 */
 
-for (let i = 10; i > 1; i += 1) {
+
+/*
+for (let i = 1; i <= 15; i += 1) {
+    if (i == 9) {
+        break;
+    }
+    console.log(i);
+}
+*/
+for (let i = 1; i <= 15; i += 1) {
+    if (i == 9) {
+        continue;
+    }
     console.log(i);
 }
 
