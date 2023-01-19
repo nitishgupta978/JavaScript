788f1571ae05dcd19a9d26939056c0baf7b95434
nitishgupta978 <nitishgupta9785@gmail.com>
2023-01-19 12:57:25 +0530

    Added : and && or || operator use

--- index.js
+++ index.js
@@ -2,25 +2,23 @@
 switch case - 
 */
 
-let grade = 55;
+let Temp = 12;
+let IndiaTemp = 60;
+let sunnyTemp = false
 
-switch (true) {
-    case grade >= 90:
-        console.log("you did greate!");
-        break;
-    case grade >= 75 && grade <= 89:
-        console.log("you did good!");
-        break;
-    case grade >= 60 && grade <= 74:
-        console.log("you did OK!");
-        break;
-    case grade >= 35 && grade <= 60:
-        console.log("you did Fine!");
-        break;
-    case grade >= 0 && grade <= 30:
-        console.log("you are FAIL !");
-        break;
-    default:
-        console.log("This is not a grade!")
+if (Temp >= 0 && Temp <= 35 && sunnyTemp) {
+    console.log("Temperature is good!")
+} else {
+    console.log("Temperature is not good!")
 }
+
+// Or || operator
+
+if (IndiaTemp >= 0 || Temp <= 35 && sunnyTemp) {
+    console.log("Temperature is good!")
+} else {
+    console.log("Temperature is not good!")
+}
+
+
 
